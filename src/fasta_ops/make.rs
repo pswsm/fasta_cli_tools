bf28c4bc01b335a750a906c8caa24614c9301ce0
Pau Figueras <pau@pswsm.cat>
2022-04-21 18:49:50 +0200

    When generating sequence, use format_str

--- src/fasta_ops/make.rs
+++ src/fasta_ops/make.rs
@@ -2,23 +2,26 @@ use std::fs::File;
 use std::io::prelude::Write;
 use std::path::PathBuf;
 use rand::seq::SliceRandom;
+use crate::fasta_ops::edit::format_str;
 
-pub fn generate(lines: u32, file: PathBuf) -> std::io::Result<String> {
+pub fn generate(bases: u32, file: PathBuf) -> std::io::Result<String> {
     let atcg: Vec<String> = vec![String::from("a"), String::from("t"), String::from("c"), String::from("g")];
-    let header: String = format!(">randomly generated sequence of {} lines\n", lines);
+    let header: String = format!(">randomly generated sequence of {} bases\n", bases);
     let mut sequence: String = String::new();
 
-    for _line in 1..=lines {
-        for _base in 1..=60 {
-            sequence.push_str(&select_rnd_str(&atcg));
-        }
-        sequence.push_str("\n");
+    for _base in 1..=bases {
+        sequence.push_str(&select_rnd_str(&atcg));
     }
+
+    let fmt_sequence: String = match format_str(sequence) {
+        Ok(seq) => seq,
+        Err(e) => panic!("Could not format. Error {}", e)
+    };
     let mut output_file = File::create(&file)?;
     output_file.write(header.as_bytes())?;
-    output_file.write(sequence.as_bytes())?;
+    output_file.write(fmt_sequence.as_bytes())?;
 
-    let result: String = format!("Generated file {} with {} bases", file.display(), lines * 60);
+    let result: String = format!("Generated file {} with {} bases", file.display(), bases * 60);
 
     Ok(result)
 }
