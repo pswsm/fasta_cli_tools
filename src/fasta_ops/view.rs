02e215e5d0bbdd77d6a18f3e2be99ab0a18599c1
pswsm <pauseawesome@protonmail.com>
2022-10-07 02:59:34 +0200

    Refactoring

--- src/fasta_ops/view.rs
+++ src/fasta_ops/view.rs
@@ -21,14 +21,9 @@ macro_rules! read2str {
     }};
 }
 
-/// Fake `Fasta` parser. Returns the contents of the file
+/// A wrapper function around `cat_f()` that returns a string.
 pub fn cat(file: &Path) -> io::Result<String> {
-    let contents: String = read2str!(file);
-    let reader_lines: std::str::Lines = contents.lines();
-    let header: String = reader_lines.into_iter().filter(|line| line.starts_with(">")).collect();
-    let sequence: String = reader_lines.into_iter().filter(|line| !(line.starts_with(">"))).collect();
-    let fasta: Fasta = Fasta::from(&[header, sequence]);
-    Ok(fasta.to_string())
+    Ok(cat_f(file).unwrap_or_else(|_| Fasta::new() ).to_string())
 }
 
 
@@ -36,33 +31,32 @@ pub fn cat(file: &Path) -> io::Result<String> {
 pub fn cat_f(file: &Path) -> Result<Fasta, io::Error> {
     let contents: String = read2str!(file);
     let reader_lines: std::str::Lines = contents.lines();
-    let header: String = reader_lines.into_iter().filter(|line| line.starts_with(">")).collect();
-    let sequence: String = reader_lines.into_iter().filter(|line| !(line.starts_with(">"))).collect();
+    let reader_lines_copy: std::str::Lines = reader_lines.clone();
+    let header: String = reader_lines.into_iter().filter(|line| line.starts_with('>')).collect();
+    let sequence: String = reader_lines_copy.into_iter().filter(|line| !(line.starts_with('>'))).collect();
 
     let fasta: Fasta = Fasta::from(&[header, sequence]);
 
     Ok(fasta)
-
 }
 
 /// Analizes the contents of a DNA or RNA sequence.
-pub fn analize(file: &Path) -> Result<String, io::Error> {
-    let fasta: Fasta = match cat_f(&file) {
+pub fn analize(file: &Path) -> Result<String, anyhow::Error> {
+    let fasta: Fasta = match cat_f(file) {
         Ok(seq) => seq,
-        Err(e) => panic!("Can't read file. Error: {}", e),
+        Err(e) => return Err(anyhow::anyhow!("Can't read file. Error: {}", e))
     };
-    let tot_chars: usize = fasta.sequence.chars().count();
-    let chars: Vec<char> = fasta.sequence.chars().collect();
-    let c_count: usize = fasta.sequence.chars().filter(|c| c.to_owned() == 'c').count();
-    let g_count: usize = fasta.sequence.chars().filter(|c| c.to_owned() == 'g').count();
-    let a_count: usize = fasta.sequence.chars().filter(|c| c.to_owned() == 'a').count();
-    let t_count: usize = fasta.sequence.chars().filter(|c| (c.to_owned() == 't' || c.to_owned() == 'u') ).count();
-    let gc_pct: f64 = ((g_count + c_count) as f64 * 100_f64) / tot_chars as f64;
-    let at_pct: f64 = ((a_count + t_count) as f64 * 100_f64) / tot_chars as f64;
+    let t_chars: usize = fasta.sequence.chars().count();
+    let c_count: usize = fasta.sequence.chars().filter(|&c| c == 'c').count();
+    let g_count: usize = fasta.sequence.chars().filter(|&c| c == 'g').count();
+    let a_count: usize = fasta.sequence.chars().filter(|&c| c == 'a').count();
+    let t_count: usize = fasta.sequence.chars().filter(|&c| (c == 't' || c == 'u') ).count();
+    let gc_pct: f64 = ((g_count + c_count) as f64 * 100_f64) / t_chars as f64;
+    let at_pct: f64 = ((a_count + t_count) as f64 * 100_f64) / t_chars as f64;
 
     let data: BTreeMap<String, String> = {
         let mut hm: BTreeMap<String, String> = BTreeMap::new();
-        hm.insert("Nucleotides".to_string(), tot_chars.to_string());
+        hm.insert("Nucleotides".to_string(), t_chars.to_string());
         hm.insert("AT Count".to_string(), (a_count + t_count).to_string());
         hm.insert("AT Percent".to_string(), at_pct.to_string());
         hm.insert("GC Count".to_string(), (c_count + g_count).to_string());
@@ -70,6 +64,6 @@ pub fn analize(file: &Path) -> Result<String, io::Error> {
         hm
     };
 
-    let result: String = data.iter().map(|(k, v)| format!("{}:\t{}\n", k, v)).collect();
+    let result: String = data.into_iter().map(|(k, v)| format!("{}:\t{}\n", k, v)).collect();
     Ok(result)
 }
