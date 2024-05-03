cfa5bb3ad3dcbabcdcb30472af3b37d40d160cf5
Pau Figueras <pau@pswsm.cat>
2024-05-03 02:32:25 +0200

    refactor(chains refactor): implement Chain trait for DNA

--- src/ctxs/dna/domain/mod.rs
+++ src/ctxs/dna/domain/mod.rs
@@ -1,11 +1,12 @@
 use anyhow::anyhow;
 
-use crate::ctxs::fasta::domain::fasta::DNA_BASES;
-
-pub type Chain = Vec<String>;
+use crate::ctxs::{
+    fasta::domain::fasta::DNA_BASES,
+    shared::domain::{Chain, ChainObject},
+};
 
 pub struct Dna {
-    chain: Chain,
+    chain: ChainObject,
 }
 
 impl TryFrom<String> for Dna {
@@ -22,6 +23,12 @@ impl TryFrom<String> for Dna {
     }
 }
 
+impl Chain for Dna {
+    fn chain_type(&self) -> crate::ctxs::shared::domain::ChainTypes {
+        crate::ctxs::shared::domain::ChainTypes::DNA
+    }
+}
+
 impl Dna {
     fn ensure_dna(value: &str) -> Result<bool, anyhow::Error> {
         match value.split("").all(|c| DNA_BASES.contains(&c)) {
@@ -30,7 +37,7 @@ impl Dna {
         }
     }
 
-    pub fn get_chain(&self) -> Chain {
+    pub fn get_chain(&self) -> ChainObject {
         self.chain.clone()
     }
 }
